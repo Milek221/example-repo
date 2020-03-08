d5f3e108ad1a9402fa9fa3227bd192989b005296
Milek221 <timus221@gmail.com>
2020-03-08 10:10:40 +0100

    pythonn

---
+++ example.py
@@ -0,0 +1,43 @@
+import random
+
+
+class Enemy():
+    def __init__(self, ancestry, gear):
+        self.enemy = ancestry
+        self.weapon = gear
+        self.hp = random.randrange(10, 20)
+        self.ac = random.randrange(12, 20)
+        self.alive = True
+
+    def fight(self, tgt):
+        print("You take a swing at the " + self.enemy + ".")
+        hit = random.randrange(0, 20)
+
+        if self.alive and hit > self.ac:
+            print("You hit the " + self.enemy + " for " + str(hit) + " damage!")
+            self.hp = self.hp - hit
+            print("The " + self.enemy + " has " + str(self.hp) + " HP remaining")
+        else:
+            print("You missed.")
+
+        if self.hp < 1:
+            self.alive = False
+
+
+# game start
+foe = Enemy("troll", "great axe")
+print("You meet a " + foe.enemy + " wielding a " + foe.weapon)
+
+# main loop
+while True:
+
+    print("Type the a key and then RETURN to attack.")
+
+    action = input()
+
+    if action.lower() == "a":
+        foe.fight(foe)
+
+    if foe.alive == False:
+        print("You have won...this time.")
+        exit()
